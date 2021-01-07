cb9d2f5d44a8c9454611a95ab32b530ec4cdd224
“Victor <victor.pacheco@gmail.com>
2021-01-07 09:47:19 -0300

    Server Update

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use(routes);
 
 app.on('pronto', () => {
   app.listen(3000, () => {
-    console.log('Acessar http://localhost:8080');
-    console.log('Servidor executando na porta 8080');
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
   });
 });
